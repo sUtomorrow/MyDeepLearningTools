09ac4e69e7debeaa65c4a6b0bd5b1f8c29319578
lty <287233266@qq.com>
2019-05-24 22:06:08 +0800

    save

--- tensorflow_keras/models/faster_rcnn/model.py
+++ tensorflow_keras/models/faster_rcnn/model.py
@@ -2,3 +2,9 @@
 # @Time     : 5/23/19 9:34 PM
 # @Author   : lty
 # @File     : model
+
+default_anchor_params = {}
+
+def FasterRCNN(backbone='vgg16', anchor_params=default_anchor_params):
+    #TODO
+    pass
