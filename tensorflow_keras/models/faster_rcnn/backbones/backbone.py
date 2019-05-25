835137bc9b2afbb922e9b8c51863f42b80e3b98d
sUtomorrow <287233266@qq.com>
2019-05-25 17:21:07 +0800

    finish vgg backbone

--- tensorflow_keras/models/faster_rcnn/backbones/backbone.py
+++ tensorflow_keras/models/faster_rcnn/backbones/backbone.py
@@ -5,16 +5,37 @@
 
 
 class Backbone(object):
+    def __init__(self):
+        # if using custom objects, the custom_objects should be updated
+        self.custom_objects = {}
+
+        self.validate()
+
+    def build_network(self):
+        """ built backbone network and set self.outputs
+        """
+        raise NotImplementedError('build_network method not implemented')
 
     def get_outputs(self):
-        """ Faster R-CNN need noly one output from backbone model
+        if hasattr(self, 'outputs'):
+            return self.outputs
+        else:
+            self.build_network()
+            return self.outputs
+
+    def get_feature_level(self):
+        """return the downsample times of model's outputs
         """
-        raise NotImplementedError('get_outputs method not implemented')
+        if hasattr(self, 'feature_level'):
+            return self.feature_level
+        else:
+            self.build_network()
+            return self.feature_level
 
     def get_custom_objects(self):
         """ return custom objects dict for this backbone
         """
-        return {}
+        return self.custom_objects
 
     def download_weights(self):
         """ Download model weights and returns path to weights file.
--- tensorflow_keras/models/faster_rcnn/backbones/vgg.py
+++ tensorflow_keras/models/faster_rcnn/backbones/vgg.py
@@ -5,7 +5,12 @@
 
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.python.keras.utils.data_utils import get_file
+from tensorflow.keras.applications import vgg16, vgg19
 from .backbone import Backbone
+
+VGG16_WEIGHTS = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+VGG19_WEIGHTS = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 class VggBackbone(Backbone):
     def __init__(self, backbone_name, inputs=None, inputs_shape=None):
@@ -14,18 +19,39 @@ class VggBackbone(Backbone):
             self.inputs = keras.Input(shape=inputs_shape)
         else:
             self.inputs = inputs
+        super(VggBackbone, self).__init__()
 
-    def get_outputs(self):
-        pass
+    def build_network(self):
+        if self.backbone_name == 'vgg16':
+            self.model = vgg16.VGG16(False, None, input_tensor=self.inputs)
+            self.outputs = self.model.get_layer('block5_pool').output()
+            self.feature_level = 5
+        elif self.backbone_name == 'vgg19':
+            self.model = vgg19.VGG19(False, None, input_tensor=self.inputs)
+            self.outputs = self.model.get_layer('block5_pool').output()
+            self.feature_level = 5
+        else:
+            raise ValueError('Backbone (\'{}\') is invalid.'.format(self.backbone_name))
 
     def download_weights(self):
-        pass
+        weights_path = None
+
+        if self.backbone_name == 'vgg16':
+            weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5', VGG16_WEIGHTS,
+                cache_subdir='models', file_hash='6d6bbae143d832006294945121d1f1fc')
+        elif self.backbone_name == 'vgg16':
+            weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5', VGG19_WEIGHTS,
+                cache_subdir='models', file_hash='253f8cb515780f3b799900260a226db6')
+        return weights_path
 
     def validate(self):
-        pass
+        allowed_backbone_names = ['vgg16', 'vgg19']
+
+        if self.backbone_name not in allowed_backbone_names:
+            raise ValueError('Backbone (\'{}\') not in allowed backbones ({}).'.format(self.backbone_name, allowed_backbone_names))
 
     def preprocess_image(self, inputs):
-        pass
-
-    def get_custom_objects(self):
-        pass
+        if self.backbone_name == 'vgg16':
+            return vgg16.preprocess_input(inputs)
+        elif self.backbone_name == 'vgg19':
+            return vgg19.preprocess_input(inputs)
